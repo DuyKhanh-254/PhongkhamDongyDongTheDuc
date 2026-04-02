b9cbb78de326d727a03454dc4842e94427616625
Nguyễn Duy Khánh <ndk254007@gmail.com>
2026-04-02 17:21:06 +0700

    chore: fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/PhongkhamDongySonTheDuc/" : "/",
+  base: mode === "production" ? "/PhongkhamDongyDongTheDuc/" : "/",
   server: {
     host: "::",
     port: 8080,
